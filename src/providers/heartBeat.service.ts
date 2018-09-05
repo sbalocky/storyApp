dd33109b279944a2e523119994d145a68f73c88f
Stan <stanislav.balocky@trivadis.com>
2018-09-05 17:37:32 +0200

    heartbeat fix

--- src/providers/heartBeat.service.ts
+++ src/providers/heartBeat.service.ts
@@ -1,25 +1,54 @@
+import { switchMap, catchError, retryWhen, delayWhen, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ToastController } from 'ionic-angular';
+import { ToastController, Toast } from 'ionic-angular';
+import { Observable, timer } from 'rxjs';
 
 @Injectable()
 export class HeartBeatService {
   heartBeatUrl = 'https://storyimagesave.azurewebsites.net/api/HeartBeat?code=yC0tddmhzwcxCriuNBlU3kqwTYC8moYdzVHwqASaM1rlewjNyMpNAA==';
   constructor(private http: HttpClient, private toastCtrl: ToastController) {}
+  hasToast: boolean = false;
+  toast: Toast = this.toastCtrl.create({
+    message: 'No Internet connection....',
+    showCloseButton: true,
+    closeButtonText: 'Ok',
+    position: 'top'
+  });
+  showToast() {
+    this.toast = this.toastCtrl.create({
+      message: 'No Internet connection....',
+      showCloseButton: true,
+      closeButtonText: 'Ok',
+      position: 'top'
+    });
+    this.toast.present();
+    this.hasToast = true;
+    this.toast.onDidDismiss(() => {
+      this.hasToast = false;
+    });
+  }
   heartBeat() {
-    this.http.get(this.heartBeatUrl).subscribe(
-      () => {
+    timer(5000, 5000)
+      .pipe(
+        switchMap(() => this.http.get(this.heartBeatUrl)),
+        retryWhen(errors =>
+          errors.pipe(
+            //log error message
+            tap(val => {
+              if (!this.hasToast) this.showToast();
+            }),
+            //restart in 5 seconds
+            delayWhen(val => {
+              console.log(val);
+              return timer(5000);
+            })
+          )
+        )
+      )
+
+      .subscribe(() => {
         console.log('internet connection Ok');
-      },
-      err => {
-        let toast = this.toastCtrl.create({
-          message: 'No Internet connection....',
-          showCloseButton: true,
-          closeButtonText: 'Ok',
-          position: 'top'
-        });
-        toast.present();
-      }
-    );
+      });
   }
 }
